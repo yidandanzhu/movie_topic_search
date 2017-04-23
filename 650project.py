addee1551db87ad50947b15897a242727e99e2a4
yidanzhu <yidanzhu@umich.edu>
2017-04-23 19:16:07 -0400

    Add files via upload

--- 650project.py
+++ 650project.py
@@ -39,16 +39,16 @@ document_setup = 40000
 synopses = head[:document_setup]
 selected_id = movie_id[:document_setup]
 
-def tokenize_and_stem(text):
-    #first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
-    tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
-    filtered_tokens = []
-    #filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
-    for token in tokens:
-        if re.search('[a-zA-Z]', token):
-            filtered_tokens.append(token)
-    stems = [stemmer.stem(t) for t in filtered_tokens]
-    return stems
+# def tokenize_and_stem(text):
+#     #first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+#     tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+#     filtered_tokens = []
+#     #filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+#     for token in tokens:
+#         if re.search('[a-zA-Z]', token):
+#             filtered_tokens.append(token)
+#     stems = [stemmer.stem(t) for t in filtered_tokens]
+#     return stems
 
 def strip_proppers(text):
     # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
